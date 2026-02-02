03379ce013e9bb735eb429aa655ee27e7ee886b3
mrabetsaid <155682332+mrabetsaid@users.noreply.github.com>
2026-02-02 14:12:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JS simple pour bouton ajouter au panier
+const btn = document.querySelector('.product-info button');
+btn.addEventListener('click', () => {
+  alert('Produit ajouté au panier !');
+});
